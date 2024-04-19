85b42645a2824a2c05bfccc03318a220152a5334
nuthanmurarysetty <inbox.nuthan@gmail.com>
2024-04-19 12:16:17 +0530

    Change in UI: Applied format of the form like name of the form and font styles.

--- PostManClone.UI/Dashboard.Designer.cs
+++ PostManClone.UI/Dashboard.Designer.cs
@@ -28,10 +28,18 @@ partial class Dashboard
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        SuspendLayout();
+        // 
+        // Dashboard
+        // 
+        AutoScaleDimensions = new SizeF(20F, 48F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(1600, 864);
+        Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        Margin = new Padding(6, 6, 6, 6);
+        Name = "Dashboard";
+        Text = "Postman Clone App By Nuthan(Nani)";
+        ResumeLayout(false);
     }
 
     #endregion
