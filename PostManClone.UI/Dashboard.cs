4693d821259e49ee73882130fd08a333ad5d15a1
nuthanmurarysetty <inbox.nuthan@gmail.com>
2024-04-29 21:51:35 +0530

    1. Added new method for valid URL in ApiAccess class. 2. Called this method on click of GO and if valid then call the actual method else showing invalid url message in status bar.

--- PostManClone.UI/Dashboard.cs
+++ PostManClone.UI/Dashboard.cs
@@ -13,15 +13,22 @@ public partial class Dashboard : Form
 
     private async void apiClick_Click(object sender, EventArgs e)
     {
+        apiStatus.Text = "Calling API...";
+        resultsText.Text = string.Empty;
+
         // async void => When it's an event
 
         // Validate API URl => We pushed this logic in Library project
         // We can reuse even if UI application is different and loosely coupled.
-
+        if (!api.IsValidUrl(apiText.Text))
+        {
+            apiStatus.Text = "Invalid Url...";
+            return;
+        }
 
         try
         {
-            apiStatus.Text = "Calling API...";
+            
 
             resultsText.Text = await api.CallApiAsync(apiText.Text);
             
--- PostmanClone.Library/ApiAccess.cs
+++ PostmanClone.Library/ApiAccess.cs
@@ -34,4 +34,15 @@ public class ApiAccess
             return $"Error: {response.StatusCode}";
         }
     }
+
+    public bool IsValidUrl(string url)
+    {
+        if(string.IsNullOrEmpty(url)) return false;
+
+        // Step 1; Uri.TryCreate(url, UriKind.Absolute, out Uri uriOutput) 
+        // Parse the url and create uri and returns true
+        // Step 2: Checks if url is https or not
+        return Uri.TryCreate(url, UriKind.Absolute, out Uri uriOutput) &&
+            (uriOutput.Scheme == Uri.UriSchemeHttps);
+    }
 }
